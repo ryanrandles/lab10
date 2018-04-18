486f002a2c1b65cb2ec55b8ce3565f4d8e6532a3
Ryan Randles <randles.ryan@gmail.com>
2018-04-17 22:07:49 -0500

    Add files via upload

--- GServer.java
+++ GServer.java
@@ -1,11 +1,18 @@
 package edu.uab.cs203.playground;
 
+import java.awt.List;
 import java.rmi.RemoteException;
 
 import edu.uab.cs203.Team;
 import edu.uab.cs203.network.GymServer;
 
 public class GServer implements GymServer {
+	
+	private Team listA;
+	private Team listB;
+	private String message;
+	private boolean Bisready;
+	private boolean Aisready;
 
 	@Override
 	public String networkToString() throws RemoteException {
@@ -14,43 +21,41 @@ public class GServer implements GymServer {
 	}
 
 	@Override
-	public void printMessage(String arg0) throws RemoteException {
+	public void printMessage(String message) throws RemoteException {
+		this.message = message;
+				System.out.println("This will, like, be a message or something..");	
+	}
+
+	@Override
+	public void registerClientA(String host, int port, String registryName) throws RemoteException {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void registerClientA(String arg0, int arg1, String arg2) throws RemoteException {
+	public void registerClientB(String host, int port, String registryName) throws RemoteException {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void registerClientB(String arg0, int arg1, String arg2) throws RemoteException {
+	public void setTeamA(Team teamA) throws RemoteException {
+		this.listA = teamA;
+	}
+
+	@Override
+	public void setTeamAReady(boolean ready) throws RemoteException {
 		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void setTeamB(Team teamB) throws RemoteException {
+		this.listB = teamB;
 		
 	}
 
 	@Override
-	public void setTeamA(Team arg0) throws RemoteException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setTeamAReady(boolean arg0) throws RemoteException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setTeamB(Team arg0) throws RemoteException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setTeamBReady(boolean arg0) throws RemoteException {
+	public void setTeamBReady(boolean ready) throws RemoteException {
 		// TODO Auto-generated method stub
 		
 	}
